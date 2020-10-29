711520bc3b232c93223e08ea9bfd96d8c042729c
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-10-29 15:48:12 +0000

    Added create/edit page

---
+++ BethanysPieShopHRM.App/Services/IJobCategoryDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.App.Services
+{
+    public interface IJobCategoryDataService
+    {
+        Task<IEnumerable<JobCategory>> GetAllJobCategories();
+        Task<JobCategory> GetJobCategoryById(int jobCategoryId);
+    }
+}
